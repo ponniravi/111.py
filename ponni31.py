1569b7f6a5cf16a1e4cf2000cbb79726f51b6d75
ponniravi <53963088+ponniravi@users.noreply.github.com>
2019-08-14 14:14:24 +0530

    Create ponni31.py

---
+++ ponni31.py
@@ -0,0 +1,13 @@
+#p
+    c=input()
+l1=len(c)
+count=0
+for i in range(0,l1):
+    if(c[i]=="("):
+        count=count+1
+    elif(c[i]==")"):
+        count=count-1
+if(count==0):
+    print("yes")
+else:
+    print("no")
